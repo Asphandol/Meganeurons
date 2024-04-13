9d90d5cf21210e3731a9534936fcc7390d1b296b
MariaOnyshchuk <onyshchuk.pn@ucu.edu.ua>
2024-04-13 12:08:11 +0300

    changed name of array

--- pyramid_array/mixtral8x7b.py
+++ pyramid_array/mixtral8x7b.py
@@ -1,4 +1,4 @@
 def pyramid(n):
     # Initialize a variable to store the pyramid
-    pyramid = [[1] * i for i in range(1, n + 1)]
-    return pyramid
+    pyramid_array = [[1] * i for i in range(1, n + 1)]
+    return pyramid_array
