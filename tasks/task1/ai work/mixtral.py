824b0485f19d7ae8107c212886ee9d39421380c2
MariaOnyshchuk <onyshchuk.pn@ucu.edu.ua>
2024-04-17 21:57:48 +0300

    "change the names of variables to more appropriate"

---
+++ tasks/task1/ai work/mixtral.py
@@ -0,0 +1,47 @@
+def create_acronym(input_sentence: str) -> str:
+    if isinstance(input_sentence, str):
+        if any(char.isdigit() for char in input_sentence):
+            return
+
+        sentences = input_sentence.split("\n")
+
+        acronyms = []
+
+        if len(sentences) > 1:
+            for sentence in sentences:
+                words = sentence.split()
+
+                acronym = "".join(word[0].upper() for word in words)
+
+                words = [acronym] + [' '.join(words)]
+
+                words = " - ".join(words)
+
+                acronyms.append(words)
+
+                if sentences.index(sentence) != (len(sentences) - 1):
+                    acronyms.append("\n")
+
+            acronyms = "".join(acronyms)
+
+        elif len(sentences) == 1:
+            sentence = sentences[0]
+
+            words = sentence.split()
+
+            acronym = ""
+
+            for word in words:
+                acronym += word[0]
+
+            acronym = acronym.upper()
+
+            acronym += " -"
+
+            words.insert(0, acronym)
+
+            acronyms = words
+
+            acronyms = " ".join(acronyms)
+
+        return acronyms
